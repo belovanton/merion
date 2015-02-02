72e373e72b108f78eac873cc8724e3ab451332e6
root <root@d8dfb0c591e0>
2015-02-02 22:07:42 +0000

    add get all names

--- server/app.js
+++ server/app.js
@@ -4,12 +4,22 @@ var application_root = __dirname,
   //  mongoose = require('mongoose');
 
 var app = express();
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
 
 var position = {
+    names: [],
     coordinatesX: [],
     coordinatesY: [],
     coordinatesZ: [],
     setCoord: function(name, X, Y, Z){
+        this.names.push(name);
         this.coordinatesX[name]=X;
         this.coordinatesY[name]=Y;
         this.coordinatesZ[name]=Z;
@@ -22,7 +32,7 @@ var position = {
             };
     },
     getAllNames: function(){
-
+        return {'names': this.names};
     }    
 }
 // Database
@@ -35,6 +45,7 @@ app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
+  app.use(allowCrossDomain);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
@@ -47,13 +58,13 @@ app.get('/api/getmyposition/:name', function (req, res){
 });
 
 app.get('/api/setmyposition/:name/:x/:y/:z', function (req, res){  
-  //employees.keys(req.params).forEach(function (key) { target[key]; })
   position.setCoord(req.params.name, req.params.x, req.params.y, req.params.z);        
-  //console.log(req.params);
   return res.send(position.getCoord(req.params.name));
-  //return;
 });
 
+app.get('/api/getallnames', function (req, res){  
+  return res.send(position.getAllNames());
+});
 // Launch server
 
 app.listen(4242);
