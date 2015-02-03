66e7524dff6bc0b10285c484abda162c622b019f
belov_ab <belov_ab@kmplzt.de>
2015-02-03 22:59:45 +0300

    add socket.io

--- server/app.js
+++ server/app.js
@@ -1,24 +1,11 @@
-var position = {
-    names: [],
-    coordinatesX: [],
-    coordinatesY: [],
-    coordinatesZ: [],
-    setCoord: function(name, X, Y, Z){
-        this.coordinatesX[name]=X;
-        this.coordinatesY[name]=Y;
-        this.coordinatesZ[name]=Z;
-    },
-    getCoord: function(name){
-        return {'name': name,
-                'X': this.coordinatesX[name],
-                'Y': this.coordinatesY[name],
-                'Z': this.coordinatesZ[name]
-            };
-    },
-    getAllNames: function(){
-        return {'names': Object.keys(this.coordinatesX)};
-    }
-}
+var object = {
+    name: '',
+    X: '',
+    Y: '',
+    Z: '',
+    type: ''
+};
+var objects = [];
 //// Database
 //
 ////mongoose.connect('mongodb://localhost/ecomm_database');
@@ -34,14 +21,11 @@ console.log('start socket');
 // Навешиваем обработчик на подключение нового клиента
 io.sockets.on('connection', function (socket) {
     // Т.к. чат простой - в качестве ников пока используем первые 5 символов от ID сокета
-    var ID = (socket.id).toString().substr(0, 5);
-    console.log(ID);
-    var time = (new Date).toLocaleTimeString();
-    // Посылаем клиенту сообщение о том, что он успешно подключился и его имя
-    socket.json.send({'event': 'connected', 'name': ID, 'time': time});
+    newObject = new object;
+    newObject.name = (socket.id).toString().substr(0, 5);
+    objects.push(newObject);
+    console.log(newObject.name);
 
-    // Посылаем всем остальным пользователям, что подключился новый клиент и его имя
-    socket.broadcast.json.send({'event': 'userJoined', 'name': ID, 'time': time});
     // Навешиваем обработчик на входящее сообщение
     socket.on('message', function (msg) {
         console.log(msg);
@@ -65,9 +49,8 @@ var events = require("events");
 (function(){
     var heartbeat = new events.EventEmitter();
     heartbeat.addListener('beat', function() {
-
-            console.log(position);
-            io.sockets.json.send(position);
+            console.log(objects);
+            io.sockets.json.send(objects);
 
         setTimeout(function(){
             heartbeat.emit('beat');
